3da6a45d73a1d83b6de77df99b2f0871270d8c26
mohammadwalikhan <mohammadwalikhan1@github.com>
2021-05-04 01:34:55 +0500

    Assignmnet Completed

---
+++ script.js
@@ -0,0 +1,85 @@
+// _______CHAPTER NO # 12-13__________
+
+        //       TASK # 1
+// var n = +prompt("enter character");
+// var A=65;
+// var Z=98;
+// var a=35;
+// var z = 5;
+// document.write(n);
+
+        //       TASK # 2
+// var aaa= +prompt("enter no1");
+// var bbb= +prompt("enter no2 ");
+// if (aaa>bbb){
+//     document.write(aaa + " " + "is larger");
+// }
+// else if (aaa===bbb){
+//     document.write(" equal");
+// }
+// else {
+//     document.write("lower");
+// }
+
+        //       TASK # 3
+// var num = +prompt("enter no 1");
+// var state = prompt("enter state");
+// if (state == "+"){
+//     document.write("number is positive");
+// }
+// else if (state == "-"){
+//     document.write("negative");
+// }
+// else {
+//     document.write("zero");
+// }
+
+        //       TASK # 4
+// var stri = [];
+//  stri = prompt("enter string");
+// if (stri[0] === "a" || stri[0] === "i" || stri[0]==="e" ){
+//     document.write("true");
+// }
+// else{
+//     document.write("false");
+// }
+
+        //       TASK # 5
+//  var setPASSWARD;
+// setPASSWARD = prompt("enter a passward");
+// var passward;
+// passward = prompt(" retype passward");
+// if (setPASSWARD === passward){
+//     document.write("login succeccfully");
+// }
+// else {
+//     document.write("incorrect passward");
+// }
+
+        //       TASK # 6
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";
+// }
+// else {
+// greeting = "Good evening";
+// }
+
+        //       TASK # 7
+// var time = +prompt("Enter a time");
+// if (time >= 0000 && time < 1200){
+//     document.write("good morning");
+// }
+// else if (time >= 1200 && time < 1700){
+//     document.write("good after noon");
+// }
+// else if (time >= 1700 && time < 2100){
+//     document.write("good evining");
+// }
+// else if (time >= 2100 && time < 2359){
+//     document.write("good night");
+// }
+// else {
+//     document.write("enter time in this formate: for example if 12.00 clock so write 1200 , if 6.00 clock so write 1700");
+// }
